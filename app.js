1b23b955351bc4ebaac5ffb0516f8f1aa71e8322
RangoSeto <hsuphyowai.mtla@gmail.com>
2022-08-23 15:17:37 +0630

    mini

---
+++ app.js
@@ -0,0 +1,21 @@
+// var getnumbers = document.querySelectorAll(".numbers");
+// for(x=0; x< getnumbers.length; x++){
+
+//     getnumbers[x].addEventListener("click",function(){
+//         this.classList.toggle('active');
+//         // if(getnumbers[x].classList.contains("active")){
+//         //     document.body.style.background="#f1f1f1";
+//         //     // this.style.display="none";
+//         //     getnumbers[x].textContent=" ";
+//         // }
+//     });
+
+//     if(getnumbers[x].classList.contains("active")){
+//         document.body.style.background="#f1f1f1";
+//         this.style.display="none";
+//         getnumbers[x].textContent=" ";
+//     }
+// }
+
+$modal-header-padding:              $modal-header-padding-y $modal-header-padding-x;
+
